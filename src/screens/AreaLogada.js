6257de4389bf98561a4fba82c4b4c13b7d87ee71
Aline Teixeira <alinemachadoteixeira@gmail.com>
2024-03-20 14:51:01 -0300

    um pouco da branch 3

--- src/screens/AreaLogada.js
+++ src/screens/AreaLogada.js
@@ -6,13 +6,11 @@ import { auth } from "../../firebase.config";
 import { signOut } from "firebase/auth";
 
 export default function AreaLogada({ navigation }) {
-  /* // Acessando dados do usuário logado
-     // console.log(auth.currentUser);
-     Para ver os dados/Objetos recuperados que vem no auth
+  /*   Para ver os dados/Objetos recuperados que vem no auth
   console.log(auth.currentUser); */
 
-  /*  recuperando e-mail para uso  */
-  const { email } = auth.currentUser;
+  /*  recuperando e-mail e displayName: apelido para uso  */
+  const { email, displayName: nome } = auth.currentUser;
   console.log(email);
 
   // Função logout para sair da conta
@@ -30,9 +28,17 @@ export default function AreaLogada({ navigation }) {
   return (
     <View style={estilos.container}>
       <View style={estilos.topo}>
-        <Text style={estilos.bemVindo}>Bem-vindo(a)</Text>
+        <Text style={estilos.bemVindo}>Bem-vindo(a) {nome || "Visitante"}</Text>
         <Text>Seu E-mail de cadastro é: </Text>
         <Text style={{ fontWeight: "bold" }}>{email} </Text>
+
+        {nome && (
+          <>
+            <Text>Nome do Usuário: </Text>
+            <Text style={{ fontWeight: "bold" }}>{nome} </Text>
+          </>
+        )}
+
         <Button onPress={logout} title="Logout" color="#D35400" />
       </View>
       <View style={estilos.geral}>
--- src/screens/Cadastro.js
+++ src/screens/Cadastro.js
@@ -1,13 +1,110 @@
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  TextInput,
+  Vibration,
+  View,
+} from "react-native";
 
-export default function Cadastro() {
+import { auth } from "../../firebase.config";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+export default function Cadastro({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [nome, setNome] = useState("");
+
+  const cadastrar = async () => {
+    if (!email || !senha || !nome) {
+      Alert.alert("Atenção", "Preencha email ou senha ou nome!");
+      Vibration.vibrate(300);
+      return;
+    }
+
+    try {
+      /* Para cadastrar o usuário na nossa Api do firebase  */
+      const contaUsuario = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        senha
+      );
+
+      // console.log(contaUsuario.user);
+
+      if (contaUsuario.user) {
+        await updateProfile(auth.currentUser, { displayName: nome });
+        console.log(contaUsuario.user.displayName);
+      }
+
+      Alert.alert("Cadastro", "Cadastro Feito com sucesso!!😁", [
+        {
+          text: "Ficar aqui mesmo!",
+          style: "cancel",
+          onPress: () => {
+            return;
+          },
+        },
+        {
+          text: "Ir para a area logada",
+          style: "default",
+          onPress: () => navigation.replace("AreaLogada"),
+        },
+      ]);
+    } catch (error) {
+      console.error(error.code);
+
+      let menssagem;
+
+      switch (error.code) {
+        case "auth/invalid-credential":
+          menssagem = "Dados Invalidos";
+          break;
+        case "auth/invalid-email":
+          menssagem = "Endereço de email invalido";
+          break;
+
+        case "auth/email-already-in-use":
+          menssagem = "Email já cadastrado";
+          break;
+
+        case "auth/weak-password":
+          menssagem = "Senha fraca (mínimo de 6 characteres)! ";
+          break;
+
+        default:
+          menssagem = "Houve um erro, tente novamente mais tarde";
+          break;
+      }
+
+      Alert.alert("Ops!", menssagem);
+    }
+  };
+
   return (
     <View style={estilos.container}>
       <View style={estilos.formulario}>
-        <TextInput placeholder="E-mail" style={estilos.input} />
-        <TextInput placeholder="Senha" style={estilos.input} secureTextEntry />
+        <TextInput
+          onChangeText={(valor) => setNome(valor)}
+          placeholder="Nome"
+          style={estilos.input}
+          keyboardType="default"
+        />
+        <TextInput
+          onChangeText={(valor) => setEmail(valor)}
+          placeholder="E-mail"
+          style={estilos.input}
+          keyboardType="email-address"
+        />
+        <TextInput
+          onChangeText={(valor) => setSenha(valor)}
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+        />
         <View style={estilos.botoes}>
-          <Button title="Cadastre-se" color="blue" />
+          <Button onPress={cadastrar} title="Cadastre-se" color="blue" />
         </View>
       </View>
     </View>
