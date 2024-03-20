ae1b3a1ff1ccbd9faba7ecd1cd141d41bd16fa1c
Aline Teixeira <alinemachadoteixeira@gmail.com>
2024-03-20 14:43:50 -0300

    Incluindo conteúdo da branch dois que faltei

--- src/screens/AreaLogada.js
+++ src/screens/AreaLogada.js
@@ -1,11 +1,39 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function AreaLogada() {
+// importação dos recursos de importação
+
+import { auth } from "../../firebase.config";
+import { signOut } from "firebase/auth";
+
+export default function AreaLogada({ navigation }) {
+  /* // Acessando dados do usuário logado
+     // console.log(auth.currentUser);
+     Para ver os dados/Objetos recuperados que vem no auth
+  console.log(auth.currentUser); */
+
+  /*  recuperando e-mail para uso  */
+  const { email } = auth.currentUser;
+  console.log(email);
+
+  // Função logout para sair da conta
+  const logout = async () => {
+    try {
+      /* Utilizamos a função signOut para remover os dados do auth 
+      no momento e enviamos para a tela inicial */
+      await signOut(auth);
+      navigation.replace("Inicial");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={estilos.container}>
       <View style={estilos.topo}>
         <Text style={estilos.bemVindo}>Bem-vindo(a)</Text>
-        <Button title="Logout" color="#D35400" />
+        <Text>Seu E-mail de cadastro é: </Text>
+        <Text style={{ fontWeight: "bold" }}>{email} </Text>
+        <Button onPress={logout} title="Logout" color="#D35400" />
       </View>
       <View style={estilos.geral}>
         <Text>Você está na área logada.</Text>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,93 @@
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  TextInput,
+  Vibration,
+  View,
+} from "react-native";
 
-export default function Login() {
+// importando os recursos de dutenticação
+import { auth } from "../../firebase.config";
+
+// importando a função de login com e-mail e senha
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+import { useState } from "react";
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const login = async () => {
+    if (!email || !senha) {
+      Alert.alert("atenção", "Preencha o email e senha");
+      Vibration.vibrate(300);
+      // Não esquecer do return para parar a execução
+      return;
+    }
+
+    try {
+      // Utilizamos essa função para verificar e validar o email e a senha que estão no firebase
+      await signInWithEmailAndPassword(auth, email, senha);
+      // Utilizamos o replace para telas onde não temos volta exemplo telas iniciais
+      navigation.replace("AreaLogada");
+    } catch (error) {
+      console.error(error.code);
+      let menssagem;
+
+      switch (error.code) {
+        case "auth/invalid-credential":
+          menssagem = "Dados Invalidos";
+          break;
+        case "auth/invalid-email":
+          menssagem = "Endereço de email invalido";
+          break;
+
+        default:
+          menssagem = "Houve um erro, tente novamente mais tarde";
+          break;
+      }
+
+      Alert.alert("Ops!", menssagem);
+    }
+  };
+
+  // Atalho: anfn -> cria arrow function sem a constante
+  const recuperarSenha = async () => {
+    try {
+      // Quando clicar no botão irá mandar um email para mudar a senha, essa função precisa do auth e email precisa ser importado
+      await sendPasswordResetEmail(auth, email);
+      Alert.alert("Recuperar Senha", "Verifique sua sua caixa de e-mails");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={estilos.container}>
       <View style={estilos.formulario}>
-        <TextInput placeholder="E-mail" style={estilos.input} />
-        <TextInput placeholder="Senha" style={estilos.input} secureTextEntry />
+        <TextInput
+          onChangeText={(valor) => setEmail(valor)}
+          placeholder="E-mail"
+          style={estilos.input}
+        />
+        <TextInput
+          onChangeText={(valor) => setSenha(valor)}
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+        />
         <View style={estilos.botoes}>
-          <Button title="Entre" color="green" />
+          <Button onPress={login} title="Entre" color="green" />
+          <Button
+            onPress={recuperarSenha}
+            title="Recuperar Senha"
+            color="gray"
+          />
         </View>
       </View>
     </View>
